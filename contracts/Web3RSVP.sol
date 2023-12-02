947d71f6b891d549e8db72673ac3c5c431a75bc0
volantxs <volantxs@gmail.com>
2023-12-03 00:58:59 +0530

    added contracts

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,34 +1,125 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract Web3RSVP{
+    struct CreateEvent {
+        bytes32 eventID;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+    mapping(bytes32 => CreateEvent) public idToEvent; //defines realtion of event id to its data
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        string calldata eventDataCID //ref to IPFS hash containg metadata
+    ) external { //visibility 
+        // generate an eventID based on other things passed in to generate a hash
+        bytes32 eventID = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
         );
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        address[] memory confirmedRSVPs; //post physical attendance
+        address[] memory claimedRSVPs; //over the dApp
+
+        // creates a new CreateEvent struct and adds it to the idToEvent mapping
+        idToEvent[eventID] = CreateEvent(
+            eventID,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false //default
+        );
+
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    
+    function  createNewRSVP(bytes32 eventId) external payable {
+        CreateEvent storage myEvent = idToEvent[eventId]; 
+        require(msg.value == myEvent.deposit, "NOT ENOUGH");
+        require(block.timestamp <= myEvent.eventTimestamp, "ALREADY HAPPENED");
+        require(
+            myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
+            "This event has reached capacity"
+        );
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require((myEvent.confirmedRSVPs[i]) != msg.sender, "ALREADY CONFIRMED");
+        }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        myEvent.confirmedRSVPs.push(payable(msg.sender));
 
-        owner.transfer(address(this).balance);
     }
+
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+        CreateEvent storage myEvent = idToEvent[eventId];
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+        address rsvpConfirm;
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            if (myEvent.confirmedRSVPs[i] == attendee){
+                rsvpConfirm = myEvent.confirmedRSVPs[i];
+            }
+        }
+        require(rsvpConfirm == attendee, "NO RSVP CONFIRM");
+         for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+            require(myEvent.claimedRSVPs[i] != attendee, "ALREADY CLAIMED");
+        }
+        require(myEvent.paidOut == false, "ALREADY PAID OUT");
+
+        myEvent.claimedRSVPs.push(attendee);
+        (bool sent,) = attendee.call{value: myEvent.deposit}("");
+        if (!sent) {
+            myEvent.claimedRSVPs.pop();
+        }
+
+        require(sent, "FAILED TO SENT ETH");
+    }
+
+    function confirmAllAttendees(bytes32 eventId) external {
+        CreateEvent memory myEvent = idToEvent[eventId];
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++ ) {
+            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+        }
+    }
+
+    function withdrawUnclaimedDeposits(bytes32 eventId) external {
+    CreateEvent memory myEvent = idToEvent[eventId];
+    require(!myEvent.paidOut, "ALREADY PAID");
+    require(
+        block.timestamp >= (myEvent.eventTimestamp + 7 days),
+        "TOO EARLY"
+    );
+    require(msg.sender == myEvent.eventOwner, "MUST BE EVENT OWNER");
+    uint256 unclaimed = myEvent.confirmedRSVPs.length - myEvent.claimedRSVPs.length;
+    uint256 payout = unclaimed * myEvent.deposit;
+    myEvent.paidOut = true;
+    (bool sent, ) = msg.sender.call{value: payout}("");
+    if (!sent) {
+        myEvent.paidOut = false;
+    }
+    require(sent, "Failed to send Ether");
+}
+
+
+
+
 }
